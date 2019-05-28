69b9ab3e1a769222df8a5fb0ac35c765c550bfb6
Medi Assumani <mediassumani49@gmail.com>
2019-05-28 10:56:38 -0700

    [Add] custom views

---
+++ OktoIDE/OktoIDE/Controller/TextEditorController.swift
@@ -0,0 +1,24 @@
+//
+//  TextEditorController.swift
+//  OktoIDE
+//
+//  Created by Medi Assumani on 5/28/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextEditorController: UIViewController {
+    
+    
+    lazy var mainTextEditor: UITextView = {
+       
+        let tv = UITextView()
+        
+        return tv
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
