aa02fc2c94615aaa9a6d387df6a3cad3e8a5129a
Medi Assumani <mediassumani49@gmail.com>
2019-05-30 19:02:35 -0700

    [Add] class to handle syntax highlighting

---
+++ OktoIDE/OktoIDE/Services/SyntaxHighlightService.swift
@@ -0,0 +1,14 @@
+//
+//  SyntaxHighlightService.swift
+//  OktoIDE
+//
+//  Created by Medi Assumani on 5/30/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct SyntaxHighlighService {
+    
+}
